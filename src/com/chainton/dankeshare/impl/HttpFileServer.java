7ec72fb1f1bbfd02de005fb9d5eb0561d7c132d2
soar <85624529@qq.com>
2014-02-13 14:55:56 +0800

    first http server success

--- src/com/chainton/dankeshare/impl/HttpFileServer.java
+++ src/com/chainton/dankeshare/impl/HttpFileServer.java
@@ -10,185 +10,238 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.os.Handler;
+
 import com.chainton.dankeshare.util.DigestUtil;
 
 /**
  * @author Administrator
- *
+ * 
  */
 public class HttpFileServer extends NanoHTTPD {
-	
+
 	private final Map<String, File> fileServiceMap;
-	private final Map<String, File> imageServiceMap;
-	
 	private static final HttpFileServer server = new HttpFileServer();
-	private static boolean  running = true;
+	private static boolean running = true;
+	private static final int HTTP_PORT = 8080;
+
+	/**
+	 * 主线程Handler实例
+	 */
+	protected Handler handler;
+
+
 	
-	public static HttpFileServer getInstance(){
+	private HttpFileServer() {
+		super(HTTP_PORT);
+		fileServiceMap = new HashMap<String, File>();
+	}
+	
+	private HttpFileServer(int port) {
+		super(port);
+		fileServiceMap = new HashMap<String, File>();
+	}
+	
+	/**
+	 * get singleton instance 
+	 * @return
+	 */
+	public static HttpFileServer getInstance() {
 		return server;
 	}
 	
-	private static final int HTTP_PORT = 8080;
-	private HttpFileServer() {
-        super(HTTP_PORT);
-        fileServiceMap = new HashMap<String, File>();
-		imageServiceMap = new HashMap<String, File>();
-    }
-
+	
 	/**
 	 * 将一个文件映射到http服务上
-	 * @param localIp 本地ip地址
-	 * @param md5	文件md5值
-	 * @param file	源文件
-	 * @return	映射的url
+	 * 
+	 * @param localIp
+	 *            本地ip地址
+	 * @param md5
+	 *            文件md5值
+	 * @param file
+	 *            源文件
+	 * @return 映射的url
 	 */
-	public String addFile(String localIp, String md5, File file){
-		String uri = "/" + md5;
+	public String addFile(String localIp, String md5, File file) {
+		
+		String fileName = file.getName();
+		int i = fileName.lastIndexOf('.');
+		String extName = fileName.substring(i);
+		
+		String uri = "/" + md5+extName;
 		String url = "http://" + localIp + ":" + HTTP_PORT + uri;
+		System.out.println("url: "+url);
+		
 		fileServiceMap.put(uri, file);
 		return url;
 	}
-	
-	/**
-	 * 将一个缩略图映射到http服务
-	 * @param localIp 本地ip地址
-	 * @param file 源文件
-	 * @return 映射的url
-	 */
-	public String addImage(String localIp, File file){
-		String md5 = DigestUtil.md5Hex(file);
-		String uri = "/" + md5;
-		String url = "http://" + localIp + ":" + HTTP_PORT + uri;
-		imageServiceMap.put(uri, file);
-		return url;
-	}
 
-	private static final Response NOT_FOUND_RESPONSE = new Response(Response.Status.NOT_FOUND, "text/html", "not found");
+
+
+	private static final Response NOT_FOUND_RESPONSE = new Response(
+			Response.Status.NOT_FOUND, "text/html", "not found");
+
 	@Override
 	public Response serve(IHTTPSession session) {
+		
+		//String type ="application/vnd.android.package-archive";
+		String type = "application/octet-stream";
 		String uri = session.getUri();
-		if(fileServiceMap.containsKey(uri)){
-			return serveFile(uri, session.getHeaders(), fileServiceMap.get(uri), "application/octet-stream");
-		} else if(imageServiceMap.containsKey(uri)){
-			return serveFile(uri, session.getHeaders(), imageServiceMap.get(uri), "image/jpeg");
-		}
+		
+		if (fileServiceMap.containsKey(uri)) {
+			return serveFile(uri, session.getHeaders(),
+					fileServiceMap.get(uri), type);
+		} 
+		
 		return NOT_FOUND_RESPONSE;
 	}
-	
-	Response serveFile(String uri, Map<String, String> header, File file, String mime) {
-        Response res;
-        try {
-            // Calculate etag
-            String etag = Integer.toHexString((file.getAbsolutePath() + file.lastModified() + "" + file.length()).hashCode());
 
-            // Support (simple) skipping:
-            long startFrom = 0;
-            long endAt = -1;
-            String range = header.get("range");
-            if (range != null) {
-                if (range.startsWith("bytes=")) {
-                    range = range.substring("bytes=".length());
-                    int minus = range.indexOf('-');
-                    try {
-                        if (minus > 0) {
-                            startFrom = Long.parseLong(range.substring(0, minus));
-                            endAt = Long.parseLong(range.substring(minus + 1));
-                        }
-                    } catch (NumberFormatException ignored) {
-                    }
-                }
-            }
+	/**
+	 * response info or files to the client
+	 * @param uri
+	 * @param header
+	 * @param file
+	 * @param mime
+	 * @return
+	 */
+	Response serveFile(String uri, Map<String, String> header, File file,
+			String mime) {
+		Response res;
+		try {
+			// Calculate etag
+			String etag = Integer.toHexString((file.getAbsolutePath()
+					+ file.lastModified() + "" + file.length()).hashCode());
 
-            // Change return code and add Content-Range header when skipping is requested
-            long fileLen = file.length();
-            if (range != null && startFrom >= 0) {
-                if (startFrom >= fileLen) {
-                    res = createResponse(Response.Status.RANGE_NOT_SATISFIABLE, NanoHTTPD.MIME_PLAINTEXT, "");
-                    res.addHeader("Content-Range", "bytes 0-0/" + fileLen);
-                    res.addHeader("ETag", etag);
-                } else {
-                    if (endAt < 0) {
-                        endAt = fileLen - 1;
-                    }
-                    long newLen = endAt - startFrom + 1;
-                    if (newLen < 0) {
-                        newLen = 0;
-                    }
+			// Support (simple) skipping:
+			long startFrom = 0;
+			long endAt = -1;
+			String range = header.get("range");
+			if (range != null) {
+				if (range.startsWith("bytes=")) {
+					range = range.substring("bytes=".length());
+					int minus = range.indexOf('-');
+					try {
+						if (minus > 0) {
+							startFrom = Long.parseLong(range
+									.substring(0, minus));
+							endAt = Long.parseLong(range.substring(minus + 1));
+						}
+					} catch (NumberFormatException ignored) {
+					}
+				}
+			}
 
-                    final long dataLen = newLen;
-                    FileInputStream fis = new FileInputStream(file) {
-                        @Override
-                        public int available() throws IOException {
-                            return (int) dataLen;
-                        }
-                    };
-                    fis.skip(startFrom);
+			// Change return code and add Content-Range header when skipping is
+			// requested
+			long fileLen = file.length();
+			if (range != null && startFrom >= 0) {
+				if (startFrom >= fileLen) {
+					res = createResponse(Response.Status.RANGE_NOT_SATISFIABLE,
+							NanoHTTPD.MIME_PLAINTEXT, "");
+					res.addHeader("Content-Range", "bytes 0-0/" + fileLen);
+					res.addHeader("ETag", etag);
+				} else {
+					if (endAt < 0) {
+						endAt = fileLen - 1;
+					}
+					long newLen = endAt - startFrom + 1;
+					if (newLen < 0) {
+						newLen = 0;
+					}
 
-                    res = createResponse(Response.Status.PARTIAL_CONTENT, mime, fis);
-                    res.addHeader("Content-Length", "" + dataLen);
-                    res.addHeader("Content-Range", "bytes " + startFrom + "-" + endAt + "/" + fileLen);
-                    res.addHeader("ETag", etag);
-                }
-            } else {
-                if (etag.equals(header.get("if-none-match")))
-                    res = createResponse(Response.Status.NOT_MODIFIED, mime, "");
-                else {
-                    res = createResponse(Response.Status.OK, mime, new FileInputStream(file));
-                    res.addHeader("Content-Length", "" + fileLen);
-                    res.addHeader("ETag", etag);
-                }
-            }
-        } catch (IOException ioe) {
-            res = createResponse(Response.Status.FORBIDDEN, NanoHTTPD.MIME_PLAINTEXT, "FORBIDDEN: Reading file failed.");
-        }
+					final long dataLen = newLen;
+					FileInputStream fis = new FileInputStream(file) {
+						@Override
+						public int available() throws IOException {
+							return (int) dataLen;
+						}
+					};
+					fis.skip(startFrom);
 
-        return res;
-    }
-	
-	private Response createResponse(Response.Status status, String mimeType, InputStream message) {
-        Response res = new Response(status, mimeType, message);
-        res.addHeader("Accept-Ranges", "bytes");
-        return res;
-    }
+					res = createResponse(Response.Status.PARTIAL_CONTENT, mime,
+							fis);
+					res.addHeader("Content-Length", "" + dataLen);
+					res.addHeader("Content-Range", "bytes " + startFrom + "-"
+							+ endAt + "/" + fileLen);
+					res.addHeader("ETag", etag);
+				}
+			} else {
+				if (etag.equals(header.get("if-none-match")))
+					res = createResponse(Response.Status.NOT_MODIFIED, mime, "");
+				else {
+					res = createResponse(Response.Status.OK, mime,
+							new FileInputStream(file));
+					res.addHeader("Content-Length", "" + fileLen);
+					res.addHeader("ETag", etag);
+				}
+			}
+		} catch (IOException ioe) {
+			res = createResponse(Response.Status.FORBIDDEN,
+					NanoHTTPD.MIME_PLAINTEXT, "FORBIDDEN: Reading file failed.");
+		}
 
-    // Announce that the file server accepts partial content requests
-    private Response createResponse(Response.Status status, String mimeType, String message) {
-        Response res = new Response(status, mimeType, message);
-        res.addHeader("Accept-Ranges", "bytes");
-        return res;
-    }
-  
-    public  void  startServer(){
-    	
-	    new Thread(){
-	    	public void run(){
-	    		 try {
-					 server.start();
-	    			// ServerRunner.executeInstance(server);
-					  System.out.println("Server started, Hit Enter to stop.\n");
-	    			 while(running){
-	    				 
-	    			 }
-	    			 System.out.println("Server stopped.\n");
+		return res;
+	}
+
+	private Response createResponse(Response.Status status, String mimeType,
+			InputStream message) {
+		Response res = new Response(status, mimeType, message);
+		res.addHeader("Accept-Ranges", "bytes");
+		return res;
+	}
+
+	// Announce that the file server accepts partial content requests
+	private Response createResponse(Response.Status status, String mimeType,
+			String message) {
+		Response res = new Response(status, mimeType, message);
+		res.addHeader("Accept-Ranges", "bytes");
+		return res;
+	}
+
+	/**
+	 * use a thread start the server;
+	 */
+	public void startServer() {
+
+		new Thread() {
+			public void run() {
+				try {
+					server.start();
+					System.out.println("Server started, Hit Enter to stop.\n");
+					while (running) {
+					}
+					System.out.println("Server stopped.\n");
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-	    	}
-	    }.start();
-    }
-    
-   
-    
-    public static void main(String[] args) {
-    	
-    	final HttpFileServer httpFileServer = HttpFileServer.getInstance();
-    	
-    	String localIp = "localhost";
+			}
+		}.start();
+	}
+	
+	/**
+	 * stop this http server
+	 */
+	public void stopServer() {
+		running = false;
+		server.stop();
+	}
+
+    /**
+     * simple test
+     * @param args
+     * @throws InterruptedException
+     */
+	public static void main(String[] args) throws InterruptedException {
+
+		final HttpFileServer httpFileServer = HttpFileServer.getInstance();
+
+		String localIp = "localhost";
 		String md5 = "k";
-		File file = new File("D:\\data.docx");
+		File file = new File("D:\\1.apk");
 		httpFileServer.addFile(localIp, md5, file);
 		httpFileServer.startServer();
-		
-    }
+
+		Thread.sleep(2000);
+		//httpFileServer.stopServer();
+	}
 }
